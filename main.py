b964a39cf746f70ac07bcbf5994c0793c0a00a73
Sarah Stillman <sarahr.stillman@gmail.com>
2025-11-07 07:33:22 -0800

    Basic loop: import pygame, init, window, log_state, flip

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import pygame
 from constants import *
+from logger import log_state
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
 
-if __name__ == "__main__":
-    main()
+
+print("Starting Asteroids!")
+print(f"Screen width: {SCREEN_WIDTH}")
+print(f"Screen height: {SCREEN_HEIGHT}")
+
+while True:
