00c666e74967cc42d0dbca6231a43f498b978e91
Sarah Stillman <sarahr.stillman@gmail.com>
2025-11-07 09:11:25 -0800

    new updates

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
-
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -20,7 +26,11 @@ def main():
                 return
         
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
